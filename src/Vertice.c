ee27313065e6ebe12a1add85ac37f4db9f1cc90a
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-03-18 14:32:57 -0300

    Vecino

--- src/Vertice.c
+++ src/Vertice.c
@@ -15,3 +15,11 @@ color Color(u32 i, Grafo g)
 
     return g->vertices[i].color;
 }
+
+u32 Vecino(u32 j, u32 i, Grafo g)
+{
+    if (i >= NumeroDeVertices(g) || j >= Grado(i, g))
+        return -1; // 2³² - 1
+
+    return g->vertices[i].vecinos[j];
+}
