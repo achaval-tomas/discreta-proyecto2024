f5ee86563f02c890541dc49b5a82c66c88c48c16
achaval-tomas <tomiachaval2013@gmail.com>
2024-04-04 11:10:07 -0300

    crear header para parte 2

---
+++ src/API2024Parte2.h
@@ -0,0 +1,13 @@
+#ifndef API2024_PARTE_2_H
+#define API2024_PARTE_2_H
+
+#include "APIG24.h"
+
+// se asume que Orden apunta a un sector de memoria con n lugares.
+u32 Greedy(Grafo G,u32* Orden);
+
+// funciones para crear ordenes.
+char GulDukat(Grafo G,u32* Orden);
+char ElimGarak(Grafo G,u32* Orden);
+
+#endif /* API2024_PARTE_2 */
