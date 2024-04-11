8109fc490bd5d722d77bc0272070d0c83afa5f88
achaval-tomas <tomiachaval2013@gmail.com>
2024-04-11 09:26:06 -0300

    Style and use color where color is due

--- src/api/GulDukat.c
+++ src/api/GulDukat.c
@@ -7,29 +7,30 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-void RevisarResultado(color* orden, u32 r, u32* m, u32* M) {
+void RevisarResultado(color* orden, u32 r, u32* m, u32* M)
+{
     char mul4 = 1;
     char even = 0;
     char odd = 0;
 
     u32 i = 0;
 
-    while (i < r-1) {
+    while (i < r - 1) {
         color c0 = orden[i];
-        color c1 = orden[i+1];
+        color c1 = orden[i + 1];
 
         if (mul4 && (c1 % 4 == 0)) {
-            assert(M[c0-1] >= M[c1-1]);
+            assert(M[c0 - 1] >= M[c1 - 1]);
         } else if (mul4 && (c1 % 4 != 0) && (c1 % 2 == 0)) {
             mul4 = 0;
             even = 1;
         } else if (even && (c1 % 2 == 0)) {
-            assert(M[c0-1] + m[c0-1] >= M[c1-1] + m[c1-1]);
+            assert(M[c0 - 1] + m[c0 - 1] >= M[c1 - 1] + m[c1 - 1]);
         } else if (even && (c1 % 2 != 0)) {
             even = 0;
             odd = 1;
         } else if (odd && (c1 % 2 == 1)) {
-            assert(m[c0-1] >= m[c1-1]);
+            assert(m[c0 - 1] >= m[c1 - 1]);
         } else {
             assert(0);
         }
@@ -53,64 +54,67 @@ static void CompletarTablas(u32* mul4, u32* par, u32* impar, u32 r, Grafo G)
     }
 
     for (u32 i = 0; i < n; ++i) {
-        u32 idx = Color(i, G) - 1;
+        u32 pos = Color(i, G) - 1;
         u32 grado = Grado(i, G);
-        M[idx] = MAX(M[idx], grado);
-        m[idx] = MIN(m[idx], grado);
+        M[pos] = MAX(M[pos], grado);
+        m[pos] = MIN(m[pos], grado);
     }
 
     for (color c = 1; c <= r; ++c) {
-        mul4[c-1] = M[c-1];
-        par[c-1] = M[c-1] + m[c-1];
-        impar[c-1] = m[c-1];
+        mul4[c - 1] = M[c - 1];
+        par[c - 1] = M[c - 1] + m[c - 1];
+        impar[c - 1] = m[c - 1];
     }
 
-    free(M); free(m);
+    free(M);
+    free(m);
 }
 
-static void ConstruirTablaCantColoresMul4(u32* tabla_cant_colores, u32* tabla_mul4, u32 n, u32 r) {
-    memset(tabla_cant_colores, 0, 2*n*sizeof(u32));
-    
-    u32 mul4 = 4;
+static void ConstruirTablaCantColoresMul4(u32* tabla_cant_colores, u32* tabla_mul4, u32 n, u32 r)
+{
+    memset(tabla_cant_colores, 0, 2 * n * sizeof(u32));
+
+    color mul4 = 4;
     while (mul4 <= r) {
-        u32 M = tabla_mul4[mul4-1];
+        u32 M = tabla_mul4[mul4 - 1];
         tabla_cant_colores[M] += 1;
         mul4 += 4;
     }
 }
 
-static void ConstruirTablaCantColoresPar(u32* tabla_cant_colores, u32* tabla_par, u32 n, u32 r) {
-    memset(tabla_cant_colores, 0, 2*n*sizeof(u32));
-    
-    u32 par = 2;
+static void ConstruirTablaCantColoresPar(u32* tabla_cant_colores, u32* tabla_par, u32 n, u32 r)
+{
+    memset(tabla_cant_colores, 0, 2 * n * sizeof(u32));
+
+    color par = 2;
     while (par <= r) {
-        u32 M_mas_m = tabla_par[par-1];
+        u32 M_mas_m = tabla_par[par - 1];
         tabla_cant_colores[M_mas_m] += 1;
         par += 4;
     }
 }
 
-static void ConstruirTablaCantColoresImpar(u32* tabla_cant_colores, u32* tabla_impar, u32 n, u32 r) {
-    memset(tabla_cant_colores, 0, 2*n*sizeof(u32));
-    
-    u32 impar = 1;
+static void ConstruirTablaCantColoresImpar(u32* tabla_cant_colores, u32* tabla_impar, u32 n, u32 r)
+{
+    memset(tabla_cant_colores, 0, 2 * n * sizeof(u32));
+
+    color impar = 1;
     while (impar <= r) {
-        u32 m = tabla_impar[impar-1];
+        u32 m = tabla_impar[impar - 1];
         tabla_cant_colores[m] += 1;
         impar += 2;
     }
 }
 
-static void ConstruirTablaPosiciones(u32* tabla_posiciones, u32* tabla_cantidad, u32 n) {
-    tabla_posiciones[2*n - 1] = 0;
-    u32 i = 2*n-2;
+static void ConstruirTablaPosiciones(u32* tabla_posiciones, u32* tabla_cantidad, u32 n)
+{
+    tabla_posiciones[2 * n - 1] = 0;
+    u32 i = 2 * n - 2;
     while (i != 0) {
-        tabla_posiciones[i] = tabla_posiciones[i+1] +
-                              tabla_cantidad[i+1];
+        tabla_posiciones[i] = tabla_posiciones[i + 1] + tabla_cantidad[i + 1];
         --i;
     }
-    tabla_posiciones[0] = tabla_posiciones[1] +
-                          tabla_cantidad[1];
+    tabla_posiciones[0] = tabla_posiciones[1] + tabla_cantidad[1];
 }
 
 /* Se asume que Orden apunta a una region de memoria con n lugares y
@@ -121,7 +125,7 @@ char GulDukat(Grafo G, u32* Orden)
 {
     char error = 0;
     u32 n = NumeroDeVertices(G);
-    u32 r = CalcularMaxColor(G);
+    color r = CalcularMaxColor(G);
 
     // tabla_mul4[i] tiene el valor M(i+1)
     u32* tabla_mul4 = calloc(r, sizeof(u32));
@@ -138,15 +142,15 @@ char GulDukat(Grafo G, u32* Orden)
     color* orden_colores = calloc(r, sizeof(color));
 
     // tabla_cantidad_colores[i] tiene la cantidad de colores con M(x) == i
-    u32* tabla_cantidad_colores = calloc(2*n, sizeof(u32));
+    u32* tabla_cantidad_colores = calloc(2 * n, sizeof(u32));
     ConstruirTablaCantColoresMul4(tabla_cantidad_colores, tabla_mul4, n, r);
 
     // tabla_posiciones[i] tiene el índice del próx color con M(x) == i
-    u32* tabla_posiciones = calloc(2*n, sizeof(u32));
+    u32* tabla_posiciones = calloc(2 * n, sizeof(u32));
     ConstruirTablaPosiciones(tabla_posiciones, tabla_cantidad_colores, n);
 
-    for (color c = 4; c <= r; c+=4) {
-        u32 M = tabla_mul4[c-1];
+    for (color c = 4; c <= r; c += 4) {
+        u32 M = tabla_mul4[c - 1];
         u32 pos = tabla_posiciones[M];
         orden_colores[pos] = c;
         tabla_posiciones[M] += 1;
@@ -157,11 +161,11 @@ char GulDukat(Grafo G, u32* Orden)
     ConstruirTablaCantColoresPar(tabla_cantidad_colores, tabla_par, n, r);
     // actualizo tabla_posiciones con esta nueva tabla de colores
     ConstruirTablaPosiciones(tabla_posiciones, tabla_cantidad_colores, n);
-    for (color c = 2; c <= r; c+=4) {
-        u32 M_mas_m = tabla_par[c-1];
+    for (color c = 2; c <= r; c += 4) {
+        u32 M_mas_m = tabla_par[c - 1];
         u32 pos = tabla_posiciones[M_mas_m];
         // sumo la cantidad de colores múltiplos de 4 porque estos están antes en el orden
-        orden_colores[pos+cantMul4] = c;
+        orden_colores[pos + cantMul4] = c;
         tabla_posiciones[M_mas_m] += 1;
     }
 
@@ -171,11 +175,11 @@ char GulDukat(Grafo G, u32* Orden)
     // actualizo tabla_posiciones con esta nueva tabla de colores
     ConstruirTablaPosiciones(tabla_posiciones, tabla_cantidad_colores, n);
 
-    for (color c = 1; c <= r; c+=2) {
-        u32 m = tabla_impar[c-1];
+    for (color c = 1; c <= r; c += 2) {
+        u32 m = tabla_impar[c - 1];
         u32 pos = tabla_posiciones[m];
         // sumo la cantidad de colores pares porque estos están antes en el orden
-        orden_colores[pos+cantPares] = c;
+        orden_colores[pos + cantPares] = c;
         tabla_posiciones[m] += 1;
     }
 
