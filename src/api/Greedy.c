5d10b42e81da6394a66c9826d9c8e03f92537633
Tomás Peyronel <tpeyronel@gmail.com>
2024-05-01 13:19:32 -0300

    controversial optimizations

--- src/api/Greedy.c
+++ src/api/Greedy.c
@@ -2,6 +2,7 @@
 #include "Math.h"
 #include "Util.h"
 #include <stdlib.h>
+#include <assert.h>
 
 // Colorear todos los vertices con 0.
 static char ResetearColores(Grafo G)
@@ -16,10 +17,11 @@ static char ResetearColores(Grafo G)
 
 /*
 Asume que el color 0 significa que el vértice no está pintado
-y que flags tiene espacio suficiente para todos los colores (delta+1).
+y que flags tiene espacio suficiente para todos los colores (delta+2).
+También se asume que flags[c] != v + 1 para todo c.
 Devuelve el mínimo color disponible para el vertice v.
 */
-static u32 ProximoColor(Grafo G, u32 v, u32 n_colores, u32* flags)
+static u32 ProximoColor(Grafo G, u32 v, u32* flags)
 {
     u32 grado = Grado(v, G);
 
@@ -27,14 +29,20 @@ static u32 ProximoColor(Grafo G, u32 v, u32 n_colores, u32* flags)
         u32 vecino = Vecino(i, v, G);
         u32 color_vecino = Color(vecino, G);
 
-        if (color_vecino > 0) {
-            flags[color_vecino - 1] = v + 1;
-        }
+        // Nos ahorramos el siguiente if:
+        // if (color_vecino != 0)
+        //     flags[color_vecino] = v + 1;
+        // Esto resulta en una buena mejora en algunos grafos como Harkon,
+        // pero también reduce el rendimiento en otros grafos como 8suPGJ.
+        flags[color_vecino] = v + 1;
     }
 
     color min_color = 1;
-    while (min_color <= n_colores && flags[min_color - 1] == v + 1)
+    // SAFE: debe ser que flags[c] != v + 1 para algún c=1, ..., Delta + 1
+    while (flags[min_color] == v + 1)
         ++min_color;
+
+    assert(min_color <= Delta(G) + 1);
 
     return min_color;
 }
@@ -43,7 +51,7 @@ static u32 ProximoColor(Grafo G, u32 v, u32 n_colores, u32* flags)
 u32 Greedy(Grafo G, u32* Orden)
 {
     u32 n = NumeroDeVertices(G);
-    
+
     // si Orden no es una biyección, retornamos error
     if (!ChequearOrden(Orden, n))
         return -1;
@@ -51,17 +59,22 @@ u32 Greedy(Grafo G, u32* Orden)
     if (ResetearColores(G))
         return -1;
 
-    // flags será utilizado por ProximoColor para buscar
-    // el menor color c disponible, (1 <= c <= delta + 1) pues
-    // Greedy colorea en <= Delta(G) + 1 colores 
-    u32* flags = calloc(Delta(G) + 1, sizeof(u32));
+    // flags será utilizado por ProximoColor para buscar.
+    // Se pone espacio para Delta(G) + 2 colores. Explicación: Greedy usará como mucho Delta + 1 colores,
+    // pues el vértice con más vecinos tiene Delta vecinos, y si cada vecino tiene un color distinto,
+    // los vecinos tendrán en total Delta colores distintos. Luego, todo vértice, independientemente de su grado, se coloreará
+    // con algún color en { 1, ..., Delta + 1 }, y por lo tanto, (a priori) esos son todos los colores posibles.
+    // Ahora bien, nosotros hacemos una optimización para ahorrar un if la cual consiste en tratar al color 0 (asignado
+    // cuando todavía no fue coloreado) como cualquier otro color, y luego al buscar el mínimo color simplemente
+    // ignorarlo. Por lo tanto se requieren Delta + 2 lugares, pues los colores posibles son { 0, ..., Delta + 1 }.
+    u32* flags = calloc(Delta(G) + 2, sizeof(u32));
     if (flags == NULL)
         return -1;
 
     u32 num_colores = 0;
     for (u32 i = 0; i < n; i++) {
         u32 v = Orden[i];
-        u32 color_v = ProximoColor(G, v, num_colores, flags);
+        u32 color_v = ProximoColor(G, v, flags);
         AsignarColor(color_v, v, G);
         num_colores = MAX(color_v, num_colores);
     }
